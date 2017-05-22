1559a46c00926ddee44f6d16c7a9e3cb0474ed8d
廖云富 <yunful@jumei.com>
2017-05-22 15:17:42 +0800

    add react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,40 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import 'normalize.css';
 import './index.less';
+
+class About extends React.Component<any, any>{
+  render() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 class App extends React.Component<any, any>{
   render() {
     return (
       <div>
-        <h1>Start Kit</h1>
+        <h1>Home Page</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+        </ul>
       </div>
     )
   }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact={true} path="/" component={App} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>,
+  document.getElementById('root'));
