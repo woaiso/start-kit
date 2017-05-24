b81cdeb7c110f549368862f5f206dcab62e7d9d1
廖云富 <yunful@jumei.com>
2017-05-24 15:51:21 +0800

    add test

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,7 +1,8 @@
+import { sum } from './../calc';
 test('adds 1 + 2 equals 3', () => {
-  expect(1 + 2).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 
 test('test 10 + 33 equals 43', () => {
-  expect(10 + 33).toBe(43);
+  expect(sum(10, 33)).toBe(43);
 })
