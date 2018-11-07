38ae342045c23671f4e742ac471673cb4bfd04e2
廖云富 <yunful@jumei.com>
2018-11-07 18:29:04 +0800

    add rxjs

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import { Link } from './../../router';
+import Rx from 'rxjs/Rx';
 
 const styles = require('./index.less');
-export default class App extends React.Component<any, any>{
+export default class App extends React.Component<any, any> {
+  componentDidMount() {
+    console.log('didMount');
+    var myObservable = new Rx.Subject();
+    myObservable.subscribe(value => console.log(value));
+    myObservable.next('foo');
+  }
   render() {
     return (
       <div>
         <h1 className={styles.title}>Home Page</h1>
         <ul>
-          <li><Link to="/about">About</Link></li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
         </ul>
       </div>
     );
